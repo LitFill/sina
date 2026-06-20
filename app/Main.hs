9b2ac902c62e4823798414bda0053fd32af2c9f0
LitFill <marrazzy54@gmail.com>
2026-06-20 09:20:18 +0700

    refactor(app/Main): split parse|infer|solve handlers, drop redundant mapLeft show

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Control.Arrow ((>>>))
 import Text.Parsec (parse)
 
 import Sina
@@ -9,20 +10,26 @@ main :: IO ()
 main = do
     let inputFile = "sample.sina"
     input <- readFile inputFile
-    either putStrLn hr $ mapLeft show $ parse parseExpr inputFile input
+    either print hParse $ parse parseExpr inputFile input
   where
-    hr :: Expr -> IO ()
-    hr expr = do
+    hParse :: Expr -> IO ()
+    hParse expr = do
         putStrLn $ "Parsed: " <> ppExpr expr
-        case runInfer expr of
+        hInfer expr
+
+    hInfer =
+        runInfer >>> \case
             Left err -> putStrLn $ "Inference error: " <> err
             Right (ty, n, cs) -> do
                 putStrLn $ "Inferred type (unresolved): " <> ppType ty
                 putStrLn $ "Fresh variable count: " <> show n
                 putStrLn $ "Constraints (" <> show (length cs) <> "):"
                 mapM_ (putStrLn . ("  " <>) . ppConstraint) cs
-                case runSolve cs of
-                    Left err -> putStrLn $ "Unification error: " <> err
-                    Right subst -> do
-                        putStrLn $ "Substitution: " <> ppSubst subst
-                        putStrLn $ "Final type: " <> ppType (apply subst ty)
+                hSolve ty cs
+
+    hSolve ty =
+        runSolve >>> \case
+            Left err -> putStrLn $ "Unification error: " <> err
+            Right subst -> do
+                putStrLn $ "Substitution: " <> ppSubst subst
+                putStrLn $ "Final type: " <> ppType (apply subst ty)
